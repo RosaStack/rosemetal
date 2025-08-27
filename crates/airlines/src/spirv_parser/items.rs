84364e971fcb92a2417ae661fd2562649b2f40e7
TheNachoBIT <nacholopezcorvalan2@gmail.com>
2025-08-27 16:53:21 -0300

    Started parsing ops

--- crates/airlines/src/spirv_parser/items.rs
+++ crates/airlines/src/spirv_parser/items.rs
@@ -6,3 +6,42 @@ pub struct SpirVSignature {
     pub bound: u32,
     pub reserved_instruction_schema: u32,
 }
+
+#[derive(Debug, Default, Clone)]
+pub struct SpirVOp {
+    pub id: u32,
+    pub value: SpirVValue,
+}
+
+#[derive(Debug, Default, Clone)]
+pub enum SpirVValue {
+    #[default]
+    Empty,
+    Capability(SpirVCapability),
+    ExtendedInstructionImport(String),
+    MemoryModel(SpirVAddressingModel, SpirVMemoryModel),
+}
+
+#[derive(Debug, Default, Clone)]
+pub enum SpirVAddressingModel {
+    #[default]
+    Logical,
+    Physical32,
+    Physical64,
+    PhysicalStorageBuffer64,
+}
+
+#[derive(Debug, Default, Clone)]
+pub enum SpirVMemoryModel {
+    #[default]
+    Simple,
+    Glsl450,
+    OpenCL,
+    Vulkan,
+}
+
+#[derive(Debug, Default, Clone)]
+pub enum SpirVCapability {
+    #[default]
+    Shader,
+}
--- crates/airlines/src/spirv_parser/mod.rs
+++ crates/airlines/src/spirv_parser/mod.rs
@@ -7,6 +7,7 @@ pub struct Parser {
     position: i64,
     signature: SpirVSignature,
     content: Vec<u8>,
+    operands: Vec<SpirVOp>,
 }
 
 impl Parser {
@@ -15,6 +16,7 @@ impl Parser {
             position: -1,
             signature: SpirVSignature::default(),
             content,
+            operands: vec![],
         }
     }
 
@@ -28,7 +30,7 @@ impl Parser {
         Ok(self.content[self.position as usize])
     }
 
-    pub fn start(&mut self) -> Result<()> {
+    pub fn get_signature(&mut self) -> Result<SpirVSignature> {
         let magic_number = u32::from_le_bytes([
             self.advance()?,
             self.advance()?,
@@ -71,13 +73,126 @@ impl Parser {
             self.advance()?,
         ]);
 
-        self.signature = SpirVSignature {
+        Ok(SpirVSignature {
             magic_number,
             version,
             generator_magic_number,
             bound,
             reserved_instruction_schema,
+        })
+    }
+
+    pub fn parse_op_capability(&mut self) -> Result<SpirVCapability> {
+        let instruction = u32::from_le_bytes([
+            self.advance()?,
+            self.advance()?,
+            self.advance()?,
+            self.advance()?,
+        ]);
+
+        Ok(match instruction {
+            1 => SpirVCapability::Shader,
+            _ => todo!(),
+        })
+    }
+
+    pub fn parse_literal(&mut self) -> Result<String> {
+        let mut byte_count = 0;
+        let mut result = String::new();
+
+        loop {
+            if byte_count == 4 {
+                byte_count = 0;
+            }
+
+            let character = self.advance()? as char;
+
+            if character == '\0' {
+                while byte_count < 3 {
+                    self.advance()?;
+                    byte_count += 1;
+                }
+
+                break;
+            }
+
+            result.push(character);
+            byte_count += 1;
+        }
+
+        Ok(result)
+    }
+
+    pub fn parse_memory_model(&mut self) -> Result<SpirVMemoryModel> {
+        let instruction = u32::from_le_bytes([
+            self.advance()?,
+            self.advance()?,
+            self.advance()?,
+            self.advance()?,
+        ]);
+
+        Ok(match instruction {
+            0 => SpirVMemoryModel::Simple,
+            1 => SpirVMemoryModel::Glsl450,
+            2 => SpirVMemoryModel::OpenCL,
+            3 => SpirVMemoryModel::Vulkan,
+            _ => return Err(anyhow!("Invalid Memory Model.")),
+        })
+    }
+
+    pub fn parse_addressing_model(&mut self) -> Result<SpirVAddressingModel> {
+        let instruction = u32::from_le_bytes([
+            self.advance()?,
+            self.advance()?,
+            self.advance()?,
+            self.advance()?,
+        ]);
+
+        Ok(match instruction {
+            0 => SpirVAddressingModel::Logical,
+            1 => SpirVAddressingModel::Physical32,
+            2 => SpirVAddressingModel::Physical64,
+            3 => SpirVAddressingModel::PhysicalStorageBuffer64,
+            _ => return Err(anyhow!("Invalid Addressing Model.")),
+        })
+    }
+
+    pub fn parse_op(&mut self) -> Result<SpirVOp> {
+        let word_count = u16::from_le_bytes([self.advance()?, self.advance()?]);
+        let op_code = u16::from_le_bytes([self.advance()?, self.advance()?]);
+
+        let mut result = SpirVOp::default();
+
+        result.value = match (op_code, word_count) {
+            (2, 17) => SpirVValue::Capability(self.parse_op_capability()?),
+            (6, 11) => {
+                result.id = u32::from_le_bytes([
+                    self.advance()?,
+                    self.advance()?,
+                    self.advance()?,
+                    self.advance()?,
+                ]);
+                SpirVValue::ExtendedInstructionImport(self.parse_literal()?)
+            }
+            (3, 14) => {
+                let addressing_model = self.parse_addressing_model()?;
+                let memory_model = self.parse_memory_model()?;
+                SpirVValue::MemoryModel(addressing_model, memory_model)
+            }
+            _ => todo!("{:?}", (op_code, word_count)),
         };
+
+        Ok(result)
+    }
+
+    pub fn start(&mut self) -> Result<()> {
+        self.signature = self.get_signature()?;
+        let mut operands: Vec<SpirVOp> = vec![];
+
+        let pos = self.position as usize;
+        while pos < self.content.len() {
+            operands.push(self.parse_op()?);
+        }
 
         todo!("{:?}", self.signature);
     }
